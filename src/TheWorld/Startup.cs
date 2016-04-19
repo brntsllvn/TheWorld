aadca673b5f329e17c114d3ee181d4ec4c5beffb
Brent Sullivan <brntsllvn@gmail.com>
2016-04-18 18:30:58 -0700

    contact form with client and server-side validation

--- src/TheWorld/Startup.cs
+++ src/TheWorld/Startup.cs
@@ -1,22 +1,50 @@
-﻿using Microsoft.AspNet.Builder;
+﻿using System;
+using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
+using TheWorld.Services;
 
 namespace TheWorld
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public static IConfigurationRoot Configuration;
+
+        public Startup(IApplicationEnvironment appEnv)
         {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(appEnv.ApplicationBasePath)
+                .AddJsonFile("config.json")            
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+#if DEBUG
+            services.AddScoped<IMailService, DebugMailService>();
+#else
+            services.AddScoped<IMailService, RealDebugMailService>();
+#endif
+        }
+
         public void Configure(IApplicationBuilder app)
         {
-            app.UseDefaultFiles();
             app.UseStaticFiles();
+
+            app.UseMvc(config =>
+            {
+                config.MapRoute(
+                  name: "Default",
+                  template: "{controller}/{action}/{id?}",
+                  defaults: new { controller = "App", action = "Index" }
+                  );
+            });
         }
 
         public static void Main(string[] args) => WebApplication.Run<Startup>(args);
