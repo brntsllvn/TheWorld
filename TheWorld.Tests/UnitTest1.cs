93db1c5a26bcc382c1d318d13da7a855c0e17980
Brent Sullivan <brntsllvn@gmail.com>
2016-04-19 09:09:27 -0700

    add xunit and simple tests

---
+++ TheWorld.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using Assert = NUnit.Framework.Assert;
+
+namespace TheWorld.Tests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void IndexReturnsAllTrips()
+        {
+            Assert.That(6, Is.EqualTo(6));
+
+        }
+    }
+}
